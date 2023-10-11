89b0baf1c7517f14caf1514c36f1d437a3f4444a
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-11 14:42:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,10 @@ def query(payload):
 user_question=st.text_input("Enter your question:")
 if user_question !="" and not user_question.strip().isspace() and not user_question == "" and not user_question.strip() == "" and not user_question.isspace():
   with st.spinner("AI Thinking...Please wait a while to Cheers!"):
-    output = query({
-      "question": user_question,
+      question_prefix+"Assistant is designed to answer user questions only based on information from USinoIP's website www.usinoip.com. If Assistant is not able to find an answer to user question, just response to the user that assistant cannot find an answer. Assistant should never make up an answer. Remember, Assistant should ONLY use information from USinoIP's website www.usinoip.com as the knowledge base."
+      input_question=question_prefix+user_question
+      output = query({
+      "question": input_question,
     })
     st.write("AI Response:")
     st.write(output)
